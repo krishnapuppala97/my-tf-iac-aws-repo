d3358bc36f36f3300e63b339cb5df57c05b80361
krishnapuppala97 <95561581+krishnapuppala97@users.noreply.github.com>
2022-12-30 16:17:28 +0530

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "crishna"
-    key    = "terraform/" - #name of the S3 object that will store the state file
+    key    = "terraform/terraformstate.tf"  #name of the S3 object that will store the state file
     region = "us-east-1"
   }
 }
