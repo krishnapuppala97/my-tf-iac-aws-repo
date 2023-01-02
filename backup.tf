fab681349234e8b9d63bd9db11a14e9fda6e94af
krishnapuppala97 <95561581+krishnapuppala97@users.noreply.github.com>
2023-01-02 15:26:36 +0530

    Delete backup.tf

--- backup.tf
+++
@@ -1,9 +0,0 @@
-/*
-terraform {
-  backend "s3" {
-    bucket = "crishna"
-    key    = "terraform/terraformstate.tf"  #name of the S3 object that will store the state file
-    region = "us-east-1"
-  }
-}
-*/
